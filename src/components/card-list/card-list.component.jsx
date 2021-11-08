95fbf414a7dcda03f53c0b7dc2fc22b960d06e8e
Harun-or-rashid <44317031+Harun-or-rashid@users.noreply.github.com>
2021-11-09 02:50:37 +0600

    robohash img added

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,5 +6,5 @@ import  './card-list.styles.css';
 export const CardList= props =>{
 
     return(<div className='card-list'>
-        {props.human.map(human => (<Card human={human} />))}
+        {props.human.map(human => (<Card key={human.id} human={human} ></Card>))}
         </div>)}
