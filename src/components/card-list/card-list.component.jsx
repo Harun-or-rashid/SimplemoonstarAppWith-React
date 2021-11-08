beae3008dfeb455ca2f2379787e7a8633197b939
Harun-or-rashid <44317031+Harun-or-rashid@users.noreply.github.com>
2021-11-09 02:15:16 +0600

    card component added  data  added by props from cardlist

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Children } from "react";
-import './card-list.styles.css';
+import {Card} from "../card/card.component"
+import  './card-list.styles.css';
 
 export const CardList= props =>{
 
-    return(<div className='card-list'>{props.human.map(human => <h2>{human.name}</h2>)}</div>)}
+    return(<div className='card-list'>
+        {props.human.map(human => (<Card human={human} />))}
+        </div>)}
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import './card.styles.css';
+
+export const Card = props=>{
+   return( 
+       <div className="">
+          <h1> {props.human.name}</h1>
+       </div>
+   )
+}
+
+
