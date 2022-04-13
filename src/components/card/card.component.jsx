f65b11d0332f00a8e008c88b21717a61bed7cd2c
Harun-or-rashid <harun35-2093@diu.edu.bd>
2022-04-13 12:53:10 +0600

    changed text

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ export const Card = props=>{
    return( 
        <div className="card-container">
         
-           <img alt='' src={'https://robohash.org/${props.monstarv.id}?set=set2&Ssize=180*180'} />
+           <img alt='' src={'https://robohash.org/${props.monstar.id}?set=set2&Ssize=180*180'} />
           <h1> {props.monstarv.name}</h1>
           <p>{props.monstarv.email}</p>
        </div>
